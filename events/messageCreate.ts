91e26234e154c8688901499c5359c80313594fae
Sugar2550 <kenken.oba@gmail.com>
2025-12-14 15:53:00 +0900

    Update messageCreate.ts

--- events/messageCreate.ts
+++ events/messageCreate.ts
@@ -1,6 +1,7 @@
 import { Message } from "discord.js";
 import { handleMemoPrefix } from "../commands/memo.js";
 import { handleIconPrefix } from "../commands/icon.js";
+import { callGAS } from "../services/gasClient.js";
 import commandsJson from "../commands/commands.json" with { type: "json" };
 
 const commands = commandsJson as Record<string, string>;
